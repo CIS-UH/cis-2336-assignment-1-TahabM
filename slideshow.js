247a4531fbaa7fbc8b008e611f983ca5f1078504
TahabM <tahabmoazzam@gmail.com>
2024-04-08 15:37:52 -0700

    assignments

--- slideshow.js
+++ slideshow.js
@@ -1,16 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Sample slideshow images (replace with your actual images)
     var slideshowImages = [
         "slide1.jpg",
         "slide2.jpeg",
         "slide3.jpg"
-        // Add more images as needed
     ];
 
-    // Get the slideshow container element
     var slideshowContainer = document.querySelector(".slideshow");
 
-    // Loop through the slideshow images and create HTML elements to display them
     slideshowImages.forEach(function(imageSrc) {
         var imageElement = document.createElement("img");
         imageElement.src = imageSrc;
